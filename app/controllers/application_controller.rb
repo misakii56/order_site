fdd38fbb9f4c3e2ed8b44e42628bdab1ae6c91a8
EC2 Default User <ec2-user@ip-172-31-29-247.ap-northeast-1.compute.internal>
2023-02-13 14:00:13 +0000

    0213

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_customer!
+ 
 
   # deviseコントローラーにストロングパラメータを追加する
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -10,5 +10,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number ])
     # アカウント編集の時にnameとprofileのストロングパラメータを追加
 
+  end
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_homes_top_path
+    when Customer
+      public_root_path
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_13_104332) do
+ActiveRecord::Schema.define(version: 2023_02_12_061544) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -53,15 +53,15 @@ ActiveRecord::Schema.define(version: 2023_02_13_104332) do
   end
 
   create_table "customers", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "last_name"
-    t.string "first_name"
-    t.string "last_name_kana"
-    t.string "first_name_kana"
-    t.string "postal_code"
-    t.string "address"
-    t.string "telephone_number"
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "telephone_number", null: false
     t.boolean "is_deleted"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -94,11 +94,6 @@ ActiveRecord::Schema.define(version: 2023_02_13_104332) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "registrations", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
