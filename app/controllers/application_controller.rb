3046fef81b56916e3b57a2e3f9995761286f9447
EC2 Default User <ec2-user@ip-172-31-29-247.ap-northeast-1.compute.internal>
2023-02-27 13:17:40 +0000

    0227

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,31 @@ class ApplicationController < ActionController::Base
 
   # deviseコントローラーにストロングパラメータを追加する
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-  def configure_permitted_parameters
-    # サインアップ時にnameのストロングパラメータを追加
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number ])
-    # アカウント編集の時にnameとprofileのストロングパラメータを追加
-
-  end
-  def after_sign_in_path_for(resource)
+   def after_sign_in_path_for(resource)
     case resource
     when Admin
       admin_homes_top_path
     when Customer
       root_path
     end
+   end
+   
+   def after_sign_up_path_for(resource)
+      my_page_path(resource)
+   end
+  def after_sign_out_path_for(resource)
+    case resource
+    when :admin
+      new_admin_session_path
+    when :customer
+      root_path
+    end
+  end
+  protected
+  def configure_permitted_parameters
+    # サインアップ時にnameのストロングパラメータを追加
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number ])
+    # アカウント編集の時にnameとprofileのストロングパラメータを追加
+
   end
 end
